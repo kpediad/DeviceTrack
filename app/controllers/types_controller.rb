57c14328d364c06256661511deeef189e8588878
kpediad <pediadk@gmail.com>
2019-04-25 20:38:43 +0300

    Adds type controller show action

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -4,11 +4,19 @@ class TypesController < ApplicationController
     if logged_in? then
       erb :'types/new'
     else
+      flash[:message] = "You need to log in first!"
       redirect_home
     end
   end
 
   get '/types/:id' do
+    if logged_in? then
+      @devices = Device.find_by(user_id: current_user.id, type_id: params[:id])
+      erb :'types/show'
+    else
+      flash[:message] = "You need to log in first!"
+      redirect_home
+    end
 
   end
 
