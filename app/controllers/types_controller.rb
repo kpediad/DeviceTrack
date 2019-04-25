635c1691edf5a21b64c7a1d68cbab32fa4626040
kpediad <pediadk@gmail.com>
2019-04-25 20:29:00 +0300

    Adds type model helper function

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,3 +1,27 @@
 class TypesController < ApplicationController
 
+  get '/types/new' do
+
+  end
+
+  get '/types/:id' do
+
+  end
+
+  post '/types/' do
+
+  end
+
+  get '/types/:id/edit' do
+
+  end
+
+  patch '/types/:id' do
+
+  end
+
+  delete '/types/:id/delete' do
+    
+  end
+
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,7 @@
 class Type < ActiveRecord::Base
   has_many :devices
+
+  def self.exists?(name)
+    User.find_by(name: name)
+  end
 end
