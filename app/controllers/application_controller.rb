9949936c04a58eb3e89b94b454a4d7f4dd9a40cb
kpediad <pediadk@gmail.com>
2019-04-25 10:04:44 +0300

    Adds index and layout template views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,23 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "b348aad8ff570de44f9420fe4a82c74fad58785afe41e1e7afd1a8c05eb5948b869f1919f5849b00705a7cb605f4fd2eb3c1896112cd3c618c73c4cf786b0ff3"
   end
 
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
   get '/' do
-    "Hello, World!!!"
+    if !logged_in?
+      erb :index, :layout => :'not_logged_in_layout' #=> Log In Page
+    else
+      erb :index
+    end
   end
 
 end
